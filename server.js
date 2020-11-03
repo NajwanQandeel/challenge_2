1a07da74881870694c071b21485ffc08432a00d3
NajwanQandeel <najwan.qandeel@gmail.com>
2020-11-03 19:54:05 +0200

    things occurred

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
-var multer  = require('multer');
-var buffer = require('buffer/').Buffer
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
+
+
+// var buffer = require('buffer/').Buffer
+// var storage = multer.memoryStorage();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -13,14 +14,18 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/client/index.html");
 });
 
-app.post("/upload", upload.single('file'), async (req, res) => {    
-  var b = req.file["buffer"]
-  console.log(b.toString())
-  res.send(b.toString())
+app.post('/upload_json', (req, res) => {
+    // console.log(req.body)
+         // JSON to CSV Converter
+         function arrayToCSV (req.body) {
+            csv = data.map(row => Object.values(row));
+            csv.unshift(Object.keys(data[0]));
+            result= csv.join('\n');
+          }
+
+    res.end(result)
 });
 
-
-
-app.listen(3000,() => {
+app.listen(3000,()=>{
     console.log("Server running on port 3000");
    });
