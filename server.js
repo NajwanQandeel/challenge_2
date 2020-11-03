dcb5cf42a0e2192a5e4d2f9c2e282dd655308291
NajwanQandeel <najwan.qandeel@gmail.com>
2020-11-03 14:26:31 +0200

    oh no

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
+const express = require('expres');
+
+let app = express();
+app.use(express.urlencoded({ extended: true })) // to parse the data
+app.use(express.json()) // to make the content header     application/json
+// app.use(express.static(__dirname + './client/index'));
+
+app.get('/upload_json', (req, res) => {
+    console.log(req);
+  })
+  
+  app.use('/',(req,res,next) => {
+    res.sendFile(path.join(__dirname,'public','index.html'));
+  });
 
 
+
+
+app.listen(3000,() => {
+    console.log("Server running on port 3000");
+   });
